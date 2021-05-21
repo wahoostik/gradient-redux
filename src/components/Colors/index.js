f4222076ae3f03cadc4f30f0d340b27c0957dc94
wahoostik <anthonypaccaud@wanadoo.fr>
2021-05-21 18:11:28 +0200

    utilisation de useSelector dans colors

--- src/components/Colors/index.js
+++ src/components/Colors/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import './styles.scss';
 
-const Colors = () => (
-  <div className="colors">
-    <span style={{ color: '#f0f' }}>#f0f</span>
-    <span style={{ transform: 'rotate(45deg)' }}>↑</span>
-    <span style={{ color: '#48b1f3' }}>#48b1f3</span>
-  </div>
-);
+const Colors = () => {
+  // Je récupère les infos du state (initialeState de index.js/reducers)
+  const theFirstColor = useSelector((state) => state.firstColor);
+  const theLastColor = useSelector((state) => state.lastColor);
+  const theDirection = useSelector((state) => state.direction);
+  // const { theFirstColor, theLastColor, theDirection } = useSelector((state) => state);
+  return (
+    <div className="colors">
+      <span style={{ color: `${theFirstColor}` }}>{theFirstColor}</span>
+      <span style={{ transform: `rotate(${theDirection})` }}>↑</span>
+      <span style={{ color: `${theLastColor}` }}>{theLastColor}</span>
+    </div>
+  );
+};
 
 export default Colors;
--- src/components/NbColors/index.js
+++ src/components/NbColors/index.js
@@ -1,9 +1,16 @@
+/* eslint-disable max-len */
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import './styles.scss';
 
-const NbColors = () => (
-  <div className="nbColors">0 couleur(s) générée(s)</div>
-);
+const NbColors = () => {
+  const nbColors = useSelector((state) => state.nbColors);
+  // Pour pouvoir lire les informations du state, react-redux nous met à disposition le hook useSelector.
+  // Il faut lui passer un callback qui prend en paramètre le state et venir lister ce qu'on lit dans le state
+  return (
+    <div className="nbColors">{nbColors} couleur(s) générée(s)</div>
+  );
+};
 
 export default NbColors;
